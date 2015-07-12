cac1306e38d6ad115c5afb22a897bc3ff312727f
Eugene Ilyin <ilyin.eugene@gmail.com>
2015-07-12 20:26:03 +0600

    Issue #2480557 by maximpodorov, lukus: Views contextual filter arguments aren't respected when ajax links are toggled

--- misc/ajax_facets.js
+++ misc/ajax_facets.js
@@ -44,16 +44,11 @@
         }
         // Check view name and display name.
         if (settings.facetapi.view_name != undefined && settings.facetapi.display_name != undefined) {
-          if (Drupal.settings && Drupal.settings.views && Drupal.settings.views.ajaxViews) {
-            // Check each rendered view.
-            $.each(Drupal.settings.views.ajaxViews, function(i, viewSettings) {
-              // If it's our view and display.
-              if (viewSettings.view_name == settings.facetapi.view_name
-                && viewSettings.view_display_id == settings.facetapi.display_name) {
-                // Take settings from view and add them into query.
-                Drupal.ajax_facets.queryState = $.extend(Drupal.ajax_facets.queryState, viewSettings);
-              }
-            });
+          Drupal.ajax_facets.queryState['view_name'] = settings.facetapi.view_name;
+          Drupal.ajax_facets.queryState['view_display_id'] = settings.facetapi.display_name;
+          var name_display = settings.facetapi.view_name + ':' + settings.facetapi.display_name;
+          if (settings.facetapi.view_args[name_display]) {
+            Drupal.ajax_facets.queryState['view_args'] = settings.facetapi.view_args[name_display];
           }
         }
 
